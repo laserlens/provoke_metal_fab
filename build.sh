89941dab30eec543c2ff29fe0b70a484880fbb0d
laserlens <laserlens@gmail.com>
2017-01-03 16:33:46 -0600

    nabber complete currently using jquerry if time allows will come aback and update nabber with angular ui instead of bootstrap

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cp node_modules/angular/angular.min.js.map public/vendors;
 
 cp node_modules/bootstrap/dist/css/bootstrap.min.css public/vendors;
 cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/vendors;
+cp node_modules/bootstrap/dist/js/bootstrap.min.js public/vendors;
 
 cp node_modules/angular-route/angular-route.min.js public/vendors;
 cp node_modules/angular-route/angular-route.min.js.map public/vendors;
