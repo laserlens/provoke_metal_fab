6dd570f600e950c2f58ce5650d7ad1cc60f4abf4
laserlens <laserlens@gmail.com>
2017-01-08 00:07:34 -0600

    fonts added button descriptions corrected,

--- public/scripts/contact.controller.js
+++ public/scripts/contact.controller.js
@@ -6,16 +6,16 @@ function ContactController(/*needed services go here*/) {
 
   /* start map view true and button name as World View */
   contact.show = true;
-  contact.buttonName = "World View";
+  contact.buttonName = "Switch to World View";
 
   /* function to allow user to to switch the boolean value of contact.show*/
   contact.viewButton = function() {
     if (contact.show === true ) {
       contact.show = false;
-      contact.buttonName = "Map View";
+      contact.buttonName = "Switch to Map View";
     } else {
       contact.show = true;
-      contact.buttonName = "World View";
+      contact.buttonName = "Switch to World View";
     }
   };
 
